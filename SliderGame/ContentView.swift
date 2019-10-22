4ed4e8fb13b5317fec60a8ce72c9f7c6a11fd820
Nilemar de Barcelos <nilemarbarcelos@gmail.com>
2019-10-22 20:14:13 +0100

    Initial Commit

---
+++ SliderGame/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  SliderGame
+//
+//  Created by Nilemar de Barcelos on 22/10/2019.
+//  Copyright © 2019 Nilemar Barcelos. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello World")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
